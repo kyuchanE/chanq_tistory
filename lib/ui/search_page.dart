6dc3ae7611777c05b755f0032701d900d45d8b13
KiU <41310752+kyuchanE@users.noreply.github.com>
2023-08-22 22:03:02 +0900

    commit

--- lib/ui/search_page.dart
+++ lib/ui/search_page.dart
@@ -3,7 +3,6 @@ import 'package:chanq_tistory_project/repository/tistory_repository.dart';
 import 'package:chanq_tistory_project/ui/main_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
 import '../model/random_user_data.dart';
 
 class SearchPage extends Page {
@@ -28,10 +27,6 @@ class SearchWidget extends StatelessWidget {
       builder: (context, state) {
         return Scaffold(
           backgroundColor: Colors.deepPurple[200],
-          floatingActionButton: ExpandableFab(
-            distance: 112,
-            children: [],
-          ),
           body: SafeArea(
             child: Stack(
               children: [
@@ -51,99 +46,6 @@ class SearchWidget extends StatelessWidget {
           ),
         );
       },
-    );
-  }
-}
-
-class ExpandableFab extends StatefulWidget {
-  final bool? initialOpen;
-  final double distance;
-  final List<Widget> children;
-
-  const ExpandableFab({
-    super.key,
-    this.initialOpen,
-    required this.distance,
-    required this.children,
-  });
-
-  @override
-  State<ExpandableFab> createState() => _ExpandableFabState();
-}
-
-class _ExpandableFabState extends State<ExpandableFab> {
-  bool _open = false;
-
-  @override
-  void initState() {
-    super.initState();
-    _open = widget.initialOpen ?? false;
-  }
-
-  void _toggle() {
-    setState(() {
-      _open = !_open;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox.expand(
-      child: Stack(
-        alignment: Alignment.bottomRight,
-        clipBehavior: Clip.none,
-        children: [_buildTapToCloseFab(), _buildTapToOpenFab()],
-      ),
-    );
-  }
-
-  Widget _buildTapToCloseFab() {
-    return SizedBox(
-      width: 56,
-      height: 56,
-      child: Center(
-        child: Material(
-          shape: const CircleBorder(),
-          clipBehavior: Clip.antiAlias,
-          elevation: 4,
-          child: InkWell(
-            onTap: _toggle,
-            child: Padding(
-              padding: const EdgeInsets.all(8),
-              child: Icon(
-                Icons.close,
-                color: Theme.of(context).primaryColor,
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildTapToOpenFab() {
-    return IgnorePointer(
-      ignoring: _open,
-      child: AnimatedContainer(
-        transformAlignment: Alignment.center,
-        transform: Matrix4.diagonal3Values(
-          _open ? 0.7 : 1.0,
-          _open ? 0.7 : 1.0,
-          _open ? 0.7 : 1.0,
-        ),
-        duration: const Duration(milliseconds: 250),
-        curve: const Interval(0.0, 0.5, curve: Curves.easeOut),
-        child: AnimatedOpacity(
-          opacity: _open ? 0.0 : 1.0,
-          curve: const Interval(0.25, 1.0, curve: Curves.easeInOut),
-          duration: const Duration(milliseconds: 250),
-          child: FloatingActionButton(
-            shape: const CircleBorder(),
-            onPressed: _toggle,
-            child: const Icon(Icons.create),
-          ),
-        ),
-      ),
     );
   }
 }
